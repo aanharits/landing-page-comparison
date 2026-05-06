9815e211a6dbb377b19c27119d0a4a224bb68133
aanharits <farhan.harits05@gmail.com>
2026-05-07 00:44:01 +0700

    refactor: remove redundant comment in vite configuration

--- react-landing/vite.config.js
+++ react-landing/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: false, // Disengaja untuk membuat bundle JavaScript React jadi jauh lebih besar
+    minify: false, 
   }
 });
