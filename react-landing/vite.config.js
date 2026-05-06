0f50d157ce5b800763d3455166a1bfc5829b29a3
aanharits <farhan.harits05@gmail.com>
2026-05-07 00:43:51 +0700

    chore: disable minification in vite config to increase react bundle size

--- react-landing/vite.config.js
+++ react-landing/vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false, // Disengaja untuk membuat bundle JavaScript React jadi jauh lebih besar
+  }
 });
